f84b6da31f11c657e8aa95cc6a6602c85cf4093a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-11 13:51:46 -0700

    more stuff in Range

--- externs/microsoft/office/interop/excel/Range.hx
+++ externs/microsoft/office/interop/excel/Range.hx
@@ -1,5 +1,16 @@
 package microsoft.office.interop.excel;
 
-extern class Range {
+extern class Range #if python implements ArrayAccess<Dynamic> #end {
   public var Value2 : Dynamic;
+  public var Formula : Dynamic;
+  var Cells(default,never) : Range;
+  var Column(default,never) : Int;
+  var Columns(default,never) : Range;
+  var Count(default,never) : Int;
+  var Row(default,never) : Int;
+  var Rows(default,never) : Range;
+  var Interior(default, never) : Interior;
+#if js
+  function Item(row : Int, col : Int) : Range;
+#end
 }
