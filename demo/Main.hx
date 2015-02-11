f0f3e35d72cb3a527a3be00c7991d73d00556e3a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-11 13:53:24 -0700

    added chart to demo

--- demo/Main.hx
+++ demo/Main.hx
@@ -1,14 +1,33 @@
 import hexcel.*;
 
+using thx.core.Arrays;
+
 class Main {
   public static function main() {
     var excel = Excel.create(),
       wb = excel.addWorkbook(),
       ws = wb.get(0),
-      range = ws.cell("A1");
+      values = [99.99, 1.11, 4.25, 98.83, 11.48, 14.58, 23.85, 85.33, 61.92, 53.74];
 
-    range.setNumber(6);
-    ws.cellAt(0, 1).setNumber(10);
-    ws.cellAt(2, 3).setNumber(666);
+    values.plucki(ws.cellAt(i, 0).setNumber(_));
+    ws.cellAt(values.length + 1, 0).setFormula('=VAR.P(A1:A${values.length})');
+
+    var range = ws.rangeAt(0, 0, values.length - 1, 0);
+    markLow(range, 10);
+
+    makeChart(ws, range);
+  }
+
+  static function markLow(range : ExcelRange, value : Float) {
+    range.map(function(cell) {
+      if(cell.getNumber() <= value) {
+        cell.setPattern(microsoft.office.interop.excel.XlPattern.xlPatternSolid);
+        cell.setThemeColor(microsoft.office.interop.excel.XlThemeColor.xlThemeColorAccent6);
+      }
+    });
+  }
+
+  static function makeChart(sheet : ExcelWorksheet, range : ExcelRange) {
+    sheet.addChart(xlColumnClustered, 120, 20, 500, 500, range);
   }
 }
